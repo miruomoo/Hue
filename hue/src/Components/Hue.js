05556b234713069064d6e5b2b2b53b2498058df2
Miruo Lin <miruomoo@gmail.com>
2023-12-02 18:11:40 -0500

    Update

---
+++ hue/src/Components/Hue.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { ReactDOM } from "react"
+
+import "../Styles/Hue.css"
+
+function Hue() {
+    return (
+      <div>
+        <div className='numbers'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      </div>
+    )
+  }
+
+export default Hue;
--- hue/src/index.js
+++ hue/src/index.js
@@ -1,81 +1,112 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
+import Hue from './Components/Hue.js'
+import './index.css';
 
-const pizzaData = [
-    {
-      name: "Focaccia",
-      ingredients: "Bread with italian olive oil and rosemary",
-      price: 6,
-      photoName: "pizzas/focaccia.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Margherita",
-      ingredients: "Tomato and mozarella",
-      price: 10,
-      photoName: "pizzas/margherita.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Spinaci",
-      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
-      price: 12,
-      photoName: "pizzas/spinaci.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Funghi",
-      ingredients: "Tomato, mozarella, mushrooms, and onion",
-      price: 12,
-      photoName: "pizzas/funghi.jpg",
-      soldOut: false,
-    },
-    {
-      name: "Pizza Salamino",
-      ingredients: "Tomato, mozarella, and pepperoni",
-      price: 15,
-      photoName: "pizzas/salamino.jpg",
-      soldOut: true,
-    },
-    {
-      name: "Pizza Prosciutto",
-      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
-      price: 18,
-      photoName: "pizzas/prosciutto.jpg",
-      soldOut: false,
-    },
-  ];
+const artistData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
-function Artist() {
-    return <div>
+function Artist({ artistObj }) {
+  return (<li className='pizza'>
+    <img src={artistObj.photoName} alt={artistObj.name} />
+    <div>
+      <h3>{artistObj.name}</h3>
+      <p>{artistObj.ingredients}</p>
+      <span>{artistObj.price}</span>
     </div>
+  </li>)
 }
 
-function Header(){
-  return <h1>Hue</h1>
+function Header() {
+  return <header className="header">
+    <h1>Your Hue</h1>
+  </header>
 }
 
-function TopArtists(){
-  return <div>
-  <p>Top Artists:</p>
-</div>
+function TopArtists() {
+  const artists = artistData;
+  const numArtists = artists.length;
+
+  return (<div>
+    <main className='menu'>
+      <h2>Top Artists:</h2>
+
+      {numArtists > 0 ? (
+        <React.Fragment>
+          <p>The artists you listened to the most this year.</p>
+          <ul className='pizzas'>
+            {artistData.map(artist =>
+              <Artist artistObj={artist} key={artist.name}
+              ></Artist>)}
+          </ul>
+        </React.Fragment>
+      ) : <p>Spotify data not fetched. :(</p>}
+    </main>
+  </div>)
 }
 
-function Footer(){
-  return (
-    <footer>{new Date().toLocaleTimeString()}</footer>
-  );
+function Footer() {
+  const check = artistData.length;
+  if (check > 0) {
+    return (
+      <footer className='footer'>
+        {new Date().toLocaleTimeString()}
+      </footer>
+    );
+  }
 }
 
 function App() {
-    return (
-        <div>
-            <Header></Header>
-            <h1>Top Artists:</h1>
-            <TopArtists></TopArtists>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div className='container'>
+      <Header></Header>
+      <TopArtists></TopArtists>
+      <Hue></Hue>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
