3a35c1ccf2b9ac392e85b7967cbb83079146a802
miruomoo <mlin282@uwo.ca>
2023-12-03 16:31:09 -0500

    Added Hue

--- hue/src/Components/Hue.js
+++ hue/src/Components/Hue.js
@@ -1,18 +1,62 @@
-import React from "react"
+import React, { useState } from "react"
 import { ReactDOM } from "react"
 
 import "../Styles/Hue.css"
 
+const messages = [
+  "Your Hue",
+  "The Meaning",
+  "Share your hue 🤑",
+];
+
 function Hue() {
+    const [step, setStep] = useState(1);
+    const [isOpen, setIsOpen] = useState(true);
+
+    function handlePrevious() {
+      if (step>1){
+        setStep((step) => step-1)
+      }
+    }
+
+    function handleNext(){
+      if (step<3){
+        setStep((step) => step+1)
+      }
+    }
+
     return (
+      <>
+      <button className="close" onClick={()=> setIsOpen((is)=> !is)}>&times;</button>
       <div>
+        {
+        isOpen && (
+        <div className="steps">
         <div className='numbers'>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={step >=1?"active":""}>1</div>
+        <div className={step >=2?"active":""}>2</div>
+        <div className={step >=3?"active":""}>3</div>
+      </div>
+
+      <p className="message">Step {step}: {messages[step-1]}</p>
+
+      <div className='buttons'>
+        <button style={{ backgroundColor: "#7950f2", color:"#fff"}} 
+        onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color:"#fff"}}
+        onClick={handleNext}>
+          Next
+        </button>
       </div>
       </div>
+        )}
+      </div>
+      </>
     )
   }
+  
 
 export default Hue;
--- hue/src/index.js
+++ hue/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-import Hue from './Components/Hue.js'
 import './index.css';
+
+import Hue from './Components/Hue.js'
 
 const artistData = [
   {
